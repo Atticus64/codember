562035e625f43ba55ab03410e613ba20a565d0de
Jona <jonathanelian64@gmail.com>
2022-11-14 19:50:30 -0600

    formating the code with prettier

--- Challenge-01/app.js
+++ Challenge-01/app.js
@@ -1,46 +1,46 @@
 import fs from 'node:fs'
 
 const input = fs.readFile('./Challenge-01/users.txt', 'utf-8', async (err, data) => {
-    if ( err ) throw err
+    if (err) throw err
     getAnswer(data)
 })
 
 const getAnswer = (value) => {
 
     const users = value
-                    .replaceAll('\n', ' ')
-                    .split(' ')
-    
+        .replaceAll('\n', ' ')
+        .split(' ')
+
 
     return filterUsers(users)
 }
 
 
-const filterUsers = ( users ) => {
-    
+const filterUsers = (users) => {
+
     const realUsers = []
     let arr = []
-    
+
     users
-        .forEach(( u ) => {
+        .forEach((u) => {
             const arrUser = u.split()
 
-            if( arrUser.includes('') ) {
+            if (arrUser.includes('')) {
                 // console.log(arr)
-                const newObject = Object.fromEntries(arr.map( u => u.split(':')))
+                const newObject = Object.fromEntries(arr.map(u => u.split(':')))
                 realUsers.push(newObject)
                 arr = []
             } else {
                 arr.push(...arrUser)
             }
 
-            
+
         })
 
 
     let count = 0
-    const res = realUsers.map( user => {
-        if ( user.usr && user.psw && user.eme && user.age && user.loc && user.fll ){
+    const res = realUsers.map(user => {
+        if (user.usr && user.psw && user.eme && user.age && user.loc && user.fll) {
             count++
             return user
         }
--- Challenge-01/main.rs
+++ Challenge-01/main.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use std::env;
+use std::fs;
 use std::vec;
 
 const REQUIRED_FIELDS: [&str; 6] = ["usr", "age", "eme", "psw", "loc", "fll"];
@@ -8,12 +8,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
 
-    let users = fs::read_to_string(file_path)
-        .expect("failed to read file");
+    let users = fs::read_to_string(file_path).expect("failed to read file");
 
     let mut users_data: Vec<String> = vec![];
-    
-    let mut user_str =  String::from("");
+
+    let mut user_str = String::from("");
     for (_idx, user) in users.lines().enumerate() {
         let content = user;
         if content != "" {
@@ -24,16 +23,17 @@ fn main() {
         }
     }
 
-    let data: Vec<String> = users_data.into_iter()
-                                        .filter(|u| have_fields(u))
-                                        .collect();
-    
-    println!("{:?}", data.len());
-    println!("{:?}", data[ data.len() -1 ]);
+    let data: Vec<String> = users_data.into_iter().filter(|u| have_fields(u)).collect();
 
+    println!("{:?}", data.len());
+    println!("{:?}", data[data.len() - 1]);
 }
 
-
 fn have_fields(data: &String) -> bool {
-    data.contains(REQUIRED_FIELDS[0]) &&  data.contains(REQUIRED_FIELDS[1]) &&  data.contains(REQUIRED_FIELDS[2]) &&  data.contains(REQUIRED_FIELDS[3]) &&  data.contains(REQUIRED_FIELDS[4]) &&  data.contains(REQUIRED_FIELDS[5])
+    data.contains(REQUIRED_FIELDS[0])
+        && data.contains(REQUIRED_FIELDS[1])
+        && data.contains(REQUIRED_FIELDS[2])
+        && data.contains(REQUIRED_FIELDS[3])
+        && data.contains(REQUIRED_FIELDS[4])
+        && data.contains(REQUIRED_FIELDS[5])
 }
