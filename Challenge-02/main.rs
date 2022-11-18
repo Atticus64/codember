f771dbaf086f88367b95f49e0813111e5a916572
Jona <jonathanelian64@gmail.com>
2022-11-18 11:21:37 -0700

    Format code in rust file challenge02

--- Challenge-02/main.rs
+++ Challenge-02/main.rs
@@ -6,36 +6,38 @@ fn main() {
     let url = "https://codember.dev/encrypted.txt";
     let res = match minreq::get(url).send() {
         Ok(res) => res,
-        Err(e) => {println!("Something bad happens: {e}"); return; }
+        Err(e) => {
+            println!("Something bad happens: {e}");
+            return;
+        }
     };
 
-    let code =  res.as_str().expect("Something failed");
+    let code = res.as_str().expect("Something failed");
     let coded_msg = String::from(code);
     println!("🔒 Coded message -> {}", coded_msg);
 
-    let result: Vec<String> = coded_msg.split(' ').map(|word| decrypt_word(word) ).collect();
-    
+    let result: Vec<String> = coded_msg
+        .split(' ')
+        .map(|word| decrypt_word(word))
+        .collect();
+
     println!("🔓🔑 Decoded message -> {}", result.join(" "));
     println!("Los criminales han sido detenidos! 🎉 ");
-
-    
 }
-
 
 /// This function will return a decoded msg
 /// receive a str of numbers -> "11688123"
 fn decrypt_word(word: &str) -> String {
     let arr: Vec<u8> = word.as_bytes().to_vec();
-    let mut new_word =  String::from("");
+    let mut new_word = String::from("");
     let mut decoded_word: String = String::from("");
     for c in arr {
         new_word.push(c as char);
         let ascii_value: u8 = new_word.parse().expect("failed to convert String to u8");
-        if ascii_value <= ASCII_Z && ascii_value >= ASCII_A  {
+        if ascii_value <= ASCII_Z && ascii_value >= ASCII_A {
             decoded_word.push(ascii_value as char);
             new_word = String::from("");
         }
     }
     return decoded_word;
-} 
-
+}
