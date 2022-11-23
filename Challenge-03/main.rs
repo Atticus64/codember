6e91c20f50367464d466ed6df492b419e49eaed7
Jona <jonathanelian64@gmail.com>
2022-11-22 22:43:17 -0700

    Add progress to challenge03 rust

--- Challenge-03/main.rs
+++ Challenge-03/main.rs
@@ -16,10 +16,38 @@ fn main() {
         .split(',')
         .collect();
 
-    println!(
-        "response: {:?}",
-        parsed_arr
-            .into_iter()
-            .for_each(|color| println!("color is {}", color))
-    );
-}
+
+    let mut last_color: String = String::from("");
+    let mut next_color: &str = parsed_arr[0];
+    let mut max_lenght_zebra: u32 = 0;
+    let mut last_zebra_color: String = String::from("");
+    let mut zebra_lenght: u32 = 0;
+
+    for color in parsed_arr.into_iter() {
+      if color != next_color && color == last_color {
+        zebra_lenght = 1;
+      } 
+
+      // let clone = color.clone();
+      
+      if color == next_color {
+        zebra_lenght = 1;
+      } else {
+        zebra_lenght += 1;
+      }
+
+      
+      // next_color = last_color.as_str();
+      last_color = color.to_string();
+      
+      if zebra_lenght > max_lenght_zebra {
+        max_lenght_zebra = zebra_lenght;
+        last_zebra_color = last_color.clone();
+      }
+      
+    }
+
+    println!("{}", last_zebra_color);
+    println!("{}", max_lenght_zebra);
+
+ }
